a24f2071a0503a8a26afab8812b58e083ee5d5de
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-07-21 00:59:27 +0100

    Rebuild fbsimctl HEAD-08451d7

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -5,12 +5,12 @@ class Fbsimctl < Formula
   sha256 "2c650fd17664fb576c450c7b55c49832b19c9faf689d175aa524142b2d0a8b2c"
   head "https://github.com/badoo/FBSimulatorControl.git"
   version "HEAD-08451d7"
-  revision 2
+  revision 0
 
   bottle do
     root_url "https://github.com/badoo/FBSimulatorControl/releases/download/08451d7"
     cellar :any
-    sha256 "d9269f3a0c2dbc2c60810c99dad5a5a71176a647c40f1a28d995aab3869d3950" => :mojave
+    sha256 "0666d83e496616d01ebf56720fb31d6a35e2dd11dc1eb9da13b6e2e1f646f75b" => :mojave
   end
 
   depends_on "carthage"
