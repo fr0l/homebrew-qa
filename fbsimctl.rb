6f2db77d036f88be0f2214506875ae8d69427831
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-07-20 23:44:06 +0100

    Add fbsimctl bottle HEAD-08451d7

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,13 +1,14 @@
 class Fbsimctl < Formula
   desc "A Powerful Command Line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/v1.5-badoo.tar.gz"
-  sha256 "3f91784f331f29cb491e736197ec8ac1435a5b41b1dd1c469162e919487c350f"
+  url "https://github.com/badoo/FBSimulatorControl/archive/08451d7.tar.gz"
+  sha256 "2c650fd17664fb576c450c7b55c49832b19c9faf689d175aa524142b2d0a8b2c"
   head "https://github.com/badoo/FBSimulatorControl.git"
-  version "1.5-badoo"
+  version "HEAD-08451d7"
+  revision 2
 
   bottle do
-    root_url "https://github.com/badoo/FBSimulatorControl/releases/download/v1.5-badoo"
+    root_url "https://github.com/badoo/FBSimulatorControl/releases/download/08451d7"
     cellar :any
     sha256 "d9269f3a0c2dbc2c60810c99dad5a5a71176a647c40f1a28d995aab3869d3950" => :mojave
   end
