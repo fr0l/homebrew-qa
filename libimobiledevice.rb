7159946f90ad5c9352f0b8f583e9634ba97773f2
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-07-29 16:59:45 +0100

    crash report filter by name

--- libimobiledevice.rb
+++ libimobiledevice.rb
@@ -1,19 +1,18 @@
 class Libimobiledevice < Formula
   desc "Library to communicate with iOS devices natively"
   homepage "https://www.libimobiledevice.org/"
-  url "https://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
-  sha256 "786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037"
-  version "HEAD-98ac7da"
-  revision 3
+  url "https://github.com/badoo/libimobiledevice/archive/8ce3312.tar.gz"
+  sha256 "26b9341162ce77b6cb3f7385217f495eb21c003178dbb10fe406f67813b7d223"
+  version "HEAD-8ce3312"
 
-  bottle do
-    root_url "https://github.com/fr0l/homebrew-qa/releases/download/badoo"
-    cellar :any
-    sha256 "8e253191b12dda7d771558c102e97f0970b856a98c788ef0accf9168c4230507" => :mojave
-  end
+  # bottle do
+  #   root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
+  #   cellar :any
+  #   sha256 "8e253191b12dda7d771558c102e97f0970b856a98c788ef0accf9168c4230507" => :mojave
+  # end
 
   head do
-    url "https://git.libimobiledevice.org/libimobiledevice.git"
+    url "https://github.com/badoo/libimobiledevice.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
