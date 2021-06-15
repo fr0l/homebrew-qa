9c0c6e88cfe3069dafb18b5492ab74654c04b9a7
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2021-06-15 19:11:09 +0100

    Update fbsimctl.rb

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,31 +1,30 @@
+# typed: false
+# frozen_string_literal: true
+
 class Fbsimctl < Formula
-  desc "A Powerful Command Line tool for automating iOS Simulators"
+  desc "Powerful Command-line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/ff11f80.tar.gz"
-  sha256 "123da36702c37cdc777e354b28405f5c004dc1d9f42b7f8067480c09be95c96a"
+  url "https://github.com/badoo/FBSimulatorControl/archive/71a72b0.tar.gz"
+  version "HEAD-71a72b0"
+  sha256 "7f8369b774d3e93142259a386c3f1ea54514f49cec0c11df8ced57750e50a6ea"
   head "https://github.com/badoo/FBSimulatorControl.git", branch: "badoo"
-  version "HEAD-ff11f80"
 
   bottle do
     root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
-    cellar :any_skip_relocation
-    sha256 "ef9636d9a5e1cc49acff1b3f3bed33283369f57f221a9fa8fecafbc022381d56" => :catalina
-    sha256 "ef9636d9a5e1cc49acff1b3f3bed33283369f57f221a9fa8fecafbc022381d56" => :mojave
-  end
-
-  depends_on "carthage"
-  depends_on :xcode => ["10.0", :build]
-
-  def install
-    system "./build.sh", "fbsimctl", "build", prefix
+    sha256 cellar: :any_skip_relocation, catalina: "310318779373d0af294b2ecd819efe99f9e96d498427ee8feb2c2a3c1c62bc37"
+    sha256 cellar: :any_skip_relocation, big_sur:  "310318779373d0af294b2ecd819efe99f9e96d498427ee8feb2c2a3c1c62bc37"
+    sha256 cellar: :any_skip_relocation, monterey:   "310318779373d0af294b2ecd819efe99f9e96d498427ee8feb2c2a3c1c62bc37"
   end
 
   pour_bottle? do
     true
   end
 
+  def install
+    raise("local builds are unsupported")
+  end
+
   test do
     system "fbsimctl", "list"
   end
 end
-
